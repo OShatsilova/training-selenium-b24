814b6bd1ada663b92b2c2350b42d529306103572
OShatsilova <oshatsilova@gmail.com>
2021-05-27 00:23:01 +0300

    add checkProductSticker test
    added StickersTests class with checkProductSticker test

---
+++ MyFirstProject/src/test/java/ru/stqa/training/selenium/litecart/StickersTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.training.selenium.litecart;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import ru.stqa.training.selenium.TestBase;
+
+import java.util.List;
+
+public class StickersTests extends TestBase {
+
+    @Test
+    public void checkProductSticker() {
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> productItems = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
+        for (int i = 0; i < productItems.size(); i++) {
+            Assert.assertTrue(String.format("У элемента %s отсутствует стикер", productItems.get(i).findElement(By.className("name")).getText()),
+                    productItems.get(i).findElements(By.xpath(".//div[contains(@class, 'sticker')]")).size() == 1);
+        }
+    }
+}
