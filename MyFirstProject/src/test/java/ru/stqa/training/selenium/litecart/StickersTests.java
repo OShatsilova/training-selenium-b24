20333aa62753d5baf23c1b5fa20d6b40654243f8
OShatsilova <oshatsilova@gmail.com>
2021-05-27 12:45:17 +0300

    edit checkProductSticker test
    edited xpath locator of productItems  element on "//ul[contains(@class, 'products')]//li[contains(@class, 'product')]"

--- MyFirstProject/src/test/java/ru/stqa/training/selenium/litecart/StickersTests.java
+++ MyFirstProject/src/test/java/ru/stqa/training/selenium/litecart/StickersTests.java
@@ -13,7 +13,7 @@ public class StickersTests extends TestBase {
     @Test
     public void checkProductSticker() {
         driver.get("http://localhost/litecart/en/");
-        List<WebElement> productItems = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
+        List<WebElement> productItems = driver.findElements(By.xpath("//ul[contains(@class, 'products')]//li[contains(@class, 'product')]"));
         for (int i = 0; i < productItems.size(); i++) {
             Assert.assertTrue(String.format("У элемента %s отсутствует стикер", productItems.get(i).findElement(By.className("name")).getText()),
                     productItems.get(i).findElements(By.xpath(".//div[contains(@class, 'sticker')]")).size() == 1);
